9b208b91dde8385ea295ee3f9cdbee05af35a93d
anhquan99 <anhquanala@gmail.com>
2023-07-31 09:34:45 +0700

    solve same tree

---
+++ Graph/DFS_SameTree.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+
+namespace Application
+{
+    public partial class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            var strP = Travel(p);
+            var strQ = Travel(q);
+            return string.Equals(strP, strQ, StringComparison.OrdinalIgnoreCase);
+        }
+        string Travel(TreeNode p)
+        {
+            StringBuilder sb = new StringBuilder();
+            InorderTraversal(p, sb);
+            return sb.ToString();
+        }
+        void InorderTraversal(TreeNode node, StringBuilder sb)
+        {
+            if (node == null)
+            {
+                sb.Append("null");
+                return;
+            }
+            sb.Append(node.val.ToString());
+            InorderTraversal(node.left, sb);
+            InorderTraversal(node.right, sb);
+        }
+    }
+}
