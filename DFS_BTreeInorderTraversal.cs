3b05b370533d670f28f24a8dd039548defe63aef
anhquan99 <anhquanala@gmail.com>
2023-07-31 08:59:08 +0700

    DFS B Tree Inorder Traversal

---
+++ DFS_BTreeInorderTraversal.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Application
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            InorderTraversal(root, result);
+            return result;
+        }
+        private void InorderTraversal(TreeNode node, List<int> result)
+        {
+            if (node == null) return;
+            InorderTraversal(node.left, result);
+            result.Add(node.val);
+            InorderTraversal(node.right, result);
+        }
+    }
+}
