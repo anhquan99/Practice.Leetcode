8c32cb8c3717225e3e24672eca22a8593e63ab39
anhquan99 <anhquanala@gmail.com>
2023-08-06 17:08:30 +0700

    resolve max depth

---
+++ Graph/MaxDepth.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application
+{
+    public partial class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            int max = 0;
+            TravelMaxDepth(root, new List<int>(), ref max);
+            return max;
+        }
+        public void TravelMaxDepth(TreeNode node, List<int> path, ref int max)
+        {
+            if (node is null) return;
+            path.Add(node.val);
+            if (max < path.Count) max = path.Count;
+            Travel(node.left, new List<int>(path), ref max);
+            Travel(node.right, new List<int>(path), ref max);
+        }
+    }
+}
