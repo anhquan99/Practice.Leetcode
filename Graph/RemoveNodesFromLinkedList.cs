572f59e3d1021b3540cdb7e713d524a7f492cadd
anhquan99 <anhquanala@gmail.com>
2024-05-28 22:43:29 +0700

    update solution

--- Graph/RemoveNodesFromLinkedList.cs
+++ Graph/RemoveNodesFromLinkedList.cs
@@ -3,6 +3,32 @@ public partial class GraphSolution
 {
     public ListNode RemoveNodes(ListNode head)
     {
-        return null;
+        var reverseNode = ReverseLinkedList(head);
+        var result = reverseNode;
+        var pre = result;
+        var node = result.next;
+        while (node != null)
+        {
+            if (node.val >= pre.val)
+            {
+                pre.next = node;
+                pre = pre.next;
+            }
+            node = node.next;
+        }
+        pre.next = null;
+        return ReverseLinkedList(result);
+    }
+    public ListNode ReverseLinkedList(ListNode head)
+    {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null)
+        {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
