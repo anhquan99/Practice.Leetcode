a3a0d50caf9a38d9675a9d0dae7245a1d4d97eaa
anhquan99 <anhquanala@gmail.com>
2024-06-09 16:24:18 +0700

    update solution

---
+++ Prefix-SuffixSum/CheckSubarraySum.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+
+namespace Application;
+public partial class PrefixSum
+{
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+        var dic = new Dictionary<int, int>();
+        dic.Add(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            var remainder = sum % k;
+            if (dic.ContainsKey(remainder) && i - dic[remainder] >= 2)
+            {
+                return true;
+            }
+            dic.TryAdd(remainder, i);
+        }
+        return false;
+    }
+}
---
+++ Prefix-SuffixSum/SubarraysDivByK.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+
+namespace Application;
+public partial class PrefixSum
+{
+    public int SubarraysDivByK(int[] nums, int k)
+    {
+        if (nums.Length == 0) return 0;
+        if (nums.Length == 1) return nums[0] % k == 0 ? 1 : 0;
+        var dic = new Dictionary<int, int>();
+        dic.Add(0, 1);
+        var count = 0;
+        var sum = 0;
+        foreach (var i in nums)
+        {
+            sum = ((sum + i) % k + k) % k; ;
+            count += dic.ContainsKey(sum) ? dic[sum] : 0;
+            if (dic.ContainsKey(sum))
+            {
+                dic[sum]++;
+            }
+            else
+            {
+                dic.Add(sum, 1);
+            }
+        }
+        return count;
+    }
+}
