6ca39af5b0c48bd829a4e28985be8b5c687cff72
anhquan99 <anhquanala@gmail.com>
2024-06-19 22:14:12 +0700

    Create CountPrefixSuffixPairs.cs

---
+++ Graph/CountPrefixSuffixPairs.cs
@@ -0,0 +1,22 @@
+namespace Application
+{
+    public partial class GraphSolution
+    {
+        public int CountPrefixSuffixPairs(string[] words)
+        {
+            var result = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i + 1; j < words.Length; j++)
+                {
+                    if (IsPrefixAndSuffix(words[i], words[j])) result++;
+                }
+            }
+            return result;
+        }
+        bool IsPrefixAndSuffix(string str1, string str2)
+        {
+            return str2.StartsWith(str1) && str2.EndsWith(str1);
+        }
+    }
+}
